9e1dbfa338bb38bab82a11483813dbc96c5e4fd5
Thittawin <ptkk00177@gmail.com>
2024-11-29 16:02:32 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int evenNum = 0 , oddNum = 0 ;
+    int i ;
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> i ;
+        
+        if (i == 0 || i < 0)
+        {
+            break;
+        }
+        
+        if (i%2 == 0){
+            evenNum++ ;
+        } 
+        else {
+            oddNum++ ;
+        }
+        
+    }
+    cout << "#Even numbers = " << evenNum << endl;
+    cout << "#Odd numbers = " << oddNum << endl;
     return 0;
 }
